58a50b0a3d86d854bb3cf8f9e0131342e510a732
Amaya Erhabor <aerhabor2005@gmail.com>
2026-08-05 20:00:01 -0500

    Add IAM policies for Rekognition access, and configure S3 bucket settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,21 @@ resource "aws_iam_user_policy_attachment" "s3_access" {
     user = aws_iam_user.rekognition_user.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+resource "aws_iam_user_policy_attachment" "rekognition_access" {
+    user = aws_iam_user.rekognition_user.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonRekognitionReadOnlyAccess"
+}
+
+resource "aws_s3_bucket" "image_bucket" {
+    bucket_prefix = "image-labels-generator-bucket-"
+}
+
+resource "aws_s3_bucket_public_access_block" "image_bucket" {
+    bucket = aws_s3_bucket.image_bucket.id
+
+    block_public_acls   = true
+    block_public_policy = true
+    ignore_public_acls =  true
+    restrict_public_buckets = true
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,11 @@ output "secret_access_key" {
     value = aws_iam_access_key.rekognition_user_key.secret
     sensitive = true
 }
+
+output "bucket_name" {
+    value = aws_s3_bucket.image_bucket.bucket
+}
+
+output "bucket_arn" {
+    value = aws_s3_bucket.image_bucket.arn
+}
