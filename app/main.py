731f9e1587b20e3dd4f38f82e5603bd2053b4b4d
Amaya Erhabor <aerhabor2005@gmail.com>
2026-08-08 02:08:18 -0500

    Add bounding boxes on detected labels

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import os, boto3
 from botocore.exceptions import ClientError 
+from PIL import Image, ImageDraw
 
 file_path = "/Users/amayasmacbook/Downloads/family_picture.png"
 bucket_name = "image-labels-generator-bucket-20260806004057421200000001"
 key = f"uploads/{os.path.basename(file_path)}"
+img = Image.open(file_path)
+img_width, img_height = img.size
 
 
 def upload_image(file_path, bucket_name, key):
@@ -19,6 +22,8 @@ def upload_image(file_path, bucket_name, key):
 
 def detect_labels(bucket, key):    
     rekognition = boto3.client('rekognition', region_name='us-east-1')
+    draw = ImageDraw.Draw(img)
+    box_dict = {}
 
     response = rekognition.detect_labels(
         Image={'S3Object': {'Bucket': bucket, 'Name': key,}},
@@ -26,11 +31,30 @@ def detect_labels(bucket, key):
         MinConfidence=75.0,
     )
 
-    print(f"Detected labels for {key}\n")
+    print(f"\nDetected labels for {key}\n")
     for label in response['Labels']:
-        print(f"Label: {label['Name']} | Confidence: {label['Confidence']:.2f}%")
+        label_text = label['Name']
+        for instance in label['Instances']:
+            box = instance['BoundingBox']
+            top_px = box['Top'] * img_height
+            left_px = box['Left'] * img_width
+            right_px = left_px + (box['Width'] * img_width)
+            bottom_px = top_px + (box['Height'] * img_height)
 
+            
+            box_dict = {
+                'Label': label_text,
+                'Box': (left_px, top_px, right_px, bottom_px),
+                'Confidence': (f"Confidence: {instance['Confidence']:.2f}%")
+            }   
+    
+            #print(f"Bounding box info: \n {box_dict}\n")
+            draw.rectangle(box_dict['Box'], fill=None, outline="black", width=3)
+        if not label['Instances']: 
+            print(f"No bounding box detected for label '{label_text}'.\n")
+
+    img.show() 
 
 if __name__ == "__main__":
-    upload_image(file_path, bucket_name, key)
+    #upload_image(file_path, bucket_name, key)
     detect_labels(bucket_name, key)
